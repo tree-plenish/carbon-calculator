cdd288f736bbd02aa1e284b95dd7906796157913
cwang360 <cntawng24@gmail.com>
2022-08-23 14:25:37 -0400

    Add requirements.txt and rename app to application

---
+++ README.md
@@ -0,0 +1,10 @@
+# carbon-calculator
+
+Because Elastic Beanstalk expects the flask app to be named `application`, set the `FLASK_APP` environment variable to `application.py` before running Flask. To run in debug mode locally, set the environment variable instead of using the parameter in `application.run()` so that the production app is not in debug mode. For example, on Linux:
+```
+$ setenv FLASK_DEBUG=1
+$ setenv FLASK_APP=application.py
+$ flask run
+```
+
+All installation dependencies required in the deployed app must be included in `requirements.txt`. If you are using a virtual environment, you can install the requirements locally from `requirements.txt` with `pip3 install -r requirements.txt` or output the installed packages in your virtual environment into `requirements.txt` with `pip3 freeze > requirements.txt`
--- application.py
+++ application.py
@@ -2,13 +2,13 @@ import math
 from flask import Flask, render_template, request, url_for,redirect, session
 
 
-app = Flask(__name__)
-app.secret_key = '3182731731231283'
+application = Flask(__name__)
+application.secret_key = '3182731731231283'
 
 square = co2 = electricityval = gasval = electricitytree = gastree = total = square_noint = co2_notint = ""
 
 
-@app.route('/', methods = ["POST", "GET"])
+@application.route('/', methods = ["POST", "GET"])
 def index():
     if request.method == "POST":
         session["square_noint"] = request.form.get("square")
@@ -20,11 +20,11 @@ def index():
     
     return render_template("index.html", squareval = electricityval, co2val = gasval, usersquare = square_noint, userco2 = co2_notint, electree=electricitytree, gtree =  gastree, total_tree = total)
 
-@app.route("/college/")
+@application.route("/college/")
 def college():
     return render_template("college.html")
 
-@app.route('/goal', methods = ["POST", "GET"])
+@application.route('/goal', methods = ["POST", "GET"])
 def goal():
     square_noint = session.get("square_noint")
     co2_notint = session.get("co2_notint")
@@ -39,5 +39,5 @@ def goal():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
     
